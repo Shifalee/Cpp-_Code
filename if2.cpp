1f8815bd170679b1c717aae23e8ee97819006c8e
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-08-19 14:16:30 +0530

    Add files via upload

---
+++ if2.cpp
@@ -0,0 +1,20 @@
+//Find the positive,zero,negative number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    if(n>0){
+        cout<<" n is positive";
+    }
+    else if(n==0)
+    {
+        cout<<" n is zero";
+    }
+    else{
+
+        cout<<" n is negative";
+    }
+}
