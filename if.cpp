dfe556a51260ce676239407d3f79700b4bbedb7d
Shifalee Khan <78851167+Shifalee@users.noreply.github.com>
2024-08-19 14:16:09 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,22 @@
+//Find Gratest number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cout<<"enter the value of a:";
+    cin>>a;
+
+    cout<<"enter the value of b:";
+    cin>>b;
+    if(a>b)
+    {
+        cout<<" a is greater "<< endl;
+    }
+    if(b>a)
+    {
+
+        cout<<" b is greater "<< endl;
+    }
+}
